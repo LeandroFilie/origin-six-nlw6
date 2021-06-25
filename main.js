32a6cad9e5b893869bc361cf1c00da961b6b4343
LeandroFilie <leandro.gf03@gmail.com>
2021-06-25 20:53:25 -0300

    Aula 5 - Porjeto Finalizado

--- main.js
+++ main.js
@@ -16,10 +16,10 @@ for (const link of links) {
   })
 }
 
-function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -34,7 +34,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 // SCROLL DIFERENTE
@@ -56,9 +62,9 @@ scrollReveal.reveal(
   { interval: 100 }
 )
 
-function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
+const backToTopButton = document.querySelector('.back-to-top')
 
+function backToTop() {
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
@@ -66,8 +72,36 @@ function backToTop() {
   }
 }
 
+// MENU ATIVO
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection(){
+  const checkpoint = window.pageYOffset + (window.innerHeight/8) * 4
+
+  for(const section of sections){
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if(checkpointStart && checkpointEnd){
+      document
+      .querySelector(`nav ul li a[href*=${sectionId}]`)
+      .classList.add('active')
+    } 
+    else{
+      document
+      .querySelector(`nav ul li a[href*=${sectionId}]`)
+      .classList.remove('active')
+    }
+
+  }
+}
+
 // EVENTO SCROLL
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection();
 })
