d1282f93844f39075ab62537ee4ad61e3bc84639
LeandroFilie <leandro.gf03@gmail.com>
2021-06-25 18:39:29 -0300

    Aula 3

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ for(const element of toggle){
   })
 }
 
-
 const links = document.querySelectorAll('nav ul li a');
 
 for(const link of links){
@@ -15,3 +14,29 @@ for(const link of links){
     nav.classList.remove('show');
   })
 }
+
+// SLIDER
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+});
+
+// ScrollReveal
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonial .testimonials,
+  #contact .text, #contact .links
+  `,{ interval: 100 })
